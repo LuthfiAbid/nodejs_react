07198ffd6b1eb19a1846cf64847751e44096702f
unknown <luthfiabid27@gmail.com>
2023-01-19 14:31:51 +0700

    Add authentication signin JWT cookier httponly

---
+++ app/controllers/auth.controller.js
@@ -0,0 +1,117 @@
+const config = require("../config/auth.config");
+const db = require("../models");
+const User = db.user;
+const Role = db.role;
+
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+
+exports.signup = (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 8)
+  });
+
+  user.save((err, user) => {
+    if (err) {
+      res.status(500).send({ msg: err });
+      return;
+    }
+
+    if (req.body.roles) {
+      Role.find(
+        {
+          name: { $in: req.body.roles },
+        },
+        (err, roles) => {
+          if (err) {
+            res.status(500).send({ msg: err });
+            return;
+          }
+
+          user.roles = roles.map((role) => role._id);
+          user.save((err) => {
+            if (err) {
+              res.status(500).send({ msg: err });
+              return;
+            }
+
+            res.send({ msg: "Register berhasil" });
+          });
+        }
+      );
+    } else {
+      Role.findOne({ name: "user" }, (err, role) => {
+        if (err) {
+          res.status(500).send({ msg: err });
+          return;
+        }
+
+        user.roles = [role._id];
+        user.save((err) => {
+          if (err) {
+            res.status(500).send({ msg: err });
+            return;
+          }
+
+          res.send({ msg: "Register berhasil" });
+        });
+      });
+    }
+  });
+}
+
+exports.signin = (req, res) => {
+  User.findOne({
+    username: req.body.username
+  })
+    .populate("roles", "-__v")
+    .exec((err, user) => {
+      if (err) {
+        res.status(500).send({ msg: err });
+        return;
+      }
+
+      if (!user) {
+        return res.status(404).send({ msg: "Username atau Password salah" });
+      }
+
+      var passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
+      if (!passwordIsValid) {
+        return res.status(404).send({ msg: "Username atau Password salah" });
+      }
+
+      var token = jwt.sign({ id: user.id }, config.secret, {
+        expiresIn: 3600 //1 jam
+      });
+
+      var authorities = [];
+
+      for (let i = 0; i < user.roles.length; i++) {
+        authorities.push("ROLE_" + user.roles[i].name.toUppercase());
+      }
+
+      req.session.token = token;
+
+      res.status(200).send({
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        roles: authorities,
+      });
+    });
+};
+
+exports.signout = async (req, res) => {
+  try {
+    req.session = null;
+    return res.status(200).send({ msg: "Anda telah logout" });
+  } catch (err) {
+    this.next(err);
+  }
+}
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const cookieSession = require("cookie-session");
 
 const app = express();
 
 var corsOptions = { origin: "http://localhost:8081" };
 
 const db = require("./app/models");
+const Role = db.role;
+
 db.mongoose.connect(db.url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
   .then(() => {
     console.log("Connected to the database!");
+    initial();
   })
   .catch(err => {
     console.log("Cannot connect to the database!", err);
@@ -27,6 +31,14 @@ app.use(bodyParser.json());
 // parse request content-type = application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(
+  cookieSession({
+    name: "example-session",
+    secret: "COOKIE_SECRET", // should use as secret environment variable
+    httpOnly: true
+  })
+);
+
 // route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome" });
@@ -36,7 +48,45 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 8080;
 
 require("./app/routes/tutorial.routes")(app);
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
